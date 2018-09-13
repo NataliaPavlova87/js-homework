34b8cef3b6ddbe351a0d2c2304ef4785a007d4dc
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-09-13 14:57:06 +0300

    создан новый класс для вложенного меню

---
+++ Submenu.js
@@ -0,0 +1,17 @@
+class subMenu {
+    constructor(id, className, items){
+        this.id = id;
+        this.className = className;
+        this.items = items;
+    }
+    rendersubMenu(){
+        let result = `<ul class="${this.className}" id="${this.id}">`;
+        for(let i = 0; i < this.items.length; i++) {
+            if (this.items[i] instanceof MenuItem){
+                result += this.items[i].renderItem();
+            }
+        }
+        result += '</ul>';
+        return result;
+    }
+}
