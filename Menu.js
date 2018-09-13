eac3af7577a5cf985635805e8a9e3154a4e4f348
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-09-13 14:55:49 +0300

    добавила проверку для Submenu

--- Menu.js
+++ Menu.js
@@ -9,6 +9,8 @@ class Menu {
         for(let i = 0; i < this.items.length; i++) {
             if (this.items[i] instanceof MenuItem){
                 result += this.items[i].renderItem();
+            } else if (this.items[i] instanceof subMenu) {
+                result += this.items[i]. rendersubMenu();
             }
         }
         result += '</ul>';
